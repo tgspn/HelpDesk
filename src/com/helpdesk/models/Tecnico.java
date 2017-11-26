f0a6b3c0c2bb5103cc787f93dc8701eb3b7d15fc
Tiago <tiago.spana@gmail.com>
2017-11-26 17:39:28 -0200

    Criação do usuário e separação do usário do tecnico

--- src/com/helpdesk/models/Tecnico.java
+++ src/com/helpdesk/models/Tecnico.java
@@ -3,68 +3,14 @@ package com.helpdesk.models;
 public class Tecnico {
 	private int id;
 	private String nome;
-	private String tipoUsuario;
-	private String usuario;
-	private String senha;
 	private int idFuncao;
-
-	public Tecnico(int id, String nome, String tipoUsuario, String usuario, String senha, int idFuncao) {
+	
+	public Tecnico(int id, String nome, int idFuncao) {
 		this.id = id;
 		this.nome = nome;
-		this.tipoUsuario = tipoUsuario;
-		this.usuario = usuario;
-		this.senha = senha;
 		this.idFuncao = idFuncao;
 	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getTipoUsuario() {
-		return tipoUsuario;
-	}
-
-	public void setTipoUsuario(String tipoUsuario) {
-		this.tipoUsuario = tipoUsuario;
-	}
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public int getIdFuncao() {
-		return idFuncao;
-	}
-
-	public void setIdFuncao(int idFuncao) {
-		this.idFuncao = idFuncao;
-	}
-
+	
 	public void CopyTo(Tecnico destino) throws Exception {
 		if (destino == null)
 			throw new Exception("O destino não pode ser nulo");
@@ -72,8 +18,44 @@ public class Tecnico {
 		destino.id = this.id;
 		destino.idFuncao = this.idFuncao;
 		destino.nome = this.nome;
-		destino.senha = this.senha;
-		destino.tipoUsuario = this.tipoUsuario;
-		destino.usuario = this.usuario;
 	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public int getIdFuncao() {
+		return idFuncao;
+	}
+
+
+
+	public void setIdFuncao(int idFuncao) {
+		this.idFuncao = idFuncao;
+	}
+
+
+	
 }
