a5baeecc0e2c25467b9e6d0b39c59984e51a14d8
Tiago <tiago.spana@gmail.com>
2017-11-26 15:14:38 -0200

    Adequação do ChamadoDAO

--- src/com/helpdesk/dao/ChamadoDAO.java
+++ src/com/helpdesk/dao/ChamadoDAO.java
@@ -1,51 +1,63 @@
 package com.helpdesk.dao;
 
+import java.sql.SQLException;
 import java.util.Comparator;
 
 import com.helpdesk.models.Chamado;
 import com.helpdesk.models.Cliente;
+import com.helpdesk.repository.ChamadoRepository;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class ChamadoDAO implements HelpdeskDAO<Chamado> {
 
+	private ChamadoRepository repository;
 	private static ObservableList<Chamado> list;
 
-	public ChamadoDAO() {
+	public ChamadoDAO() throws ClassNotFoundException {
+		repository = new ChamadoRepository();
 		if (list == null)
 			list = FXCollections.observableArrayList();
 	}
 
 	@Override
-	public void Insert(Chamado model) {
-		int lastIndex = 0;
-		if (list.stream().count() > 0) {
-			lastIndex = list.stream().max(Comparator.comparingInt(x -> x.getId())).get().getId()+1;
-		}
-		model.setId(lastIndex);
-		list.add(model);
+	public void Insert(Chamado model) throws SQLException {
+		// int lastIndex = 0;
+		// if (list.stream().count() > 0) {
+		// lastIndex = list.stream().max(Comparator.comparingInt(x ->
+		// x.getId())).get().getId()+1;
+		// }
+		// model.setId(lastIndex);
+		int result = repository.Insert(model);
+		List();
 	}
 
 	@Override
-	public void Remove(Chamado model) {
+	public void Remove(Chamado model) throws SQLException {
+		
+		repository.Delete(model.getId());
+		
 		list.remove(model);
 	}
 
 	@Override
-	public void Update(Chamado model) {
-		Chamado modelOriginal = list.stream().filter(x -> x.getId() == model.getId()).findFirst().get();
-		try {
-			model.CopyTo(modelOriginal);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+	public void Update(Chamado model) throws SQLException {
+		// Chamado modelOriginal = list.stream().filter(x -> x.getId() ==
+		// model.getId()).findFirst().get();
+		// try {
+		// model.CopyTo(modelOriginal);
+		// } catch (Exception e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		repository.Update(model);
 	}
 
 	@Override
-	public ObservableList<Chamado> List() {
+	public ObservableList<Chamado> List() throws SQLException {
+		list.clear();
+		list.addAll(repository.findAll());
 		return list;
 	}
 
--- src/com/helpdesk/dao/HelpdeskDAO.java
+++ src/com/helpdesk/dao/HelpdeskDAO.java
@@ -1,12 +1,17 @@
 package com.helpdesk.dao;
 
+import java.sql.SQLException;
+
 import javafx.collections.ObservableList;
 
 public interface HelpdeskDAO<T> {
 
-	public void Insert(T inp);
-	public void Remove(T inp);
-	public void Update(T inp);
-	public ObservableList<T> List();
-	
+	public void Insert(T inp) throws SQLException;
+
+	public void Remove(T inp) throws SQLException;
+
+	public void Update(T inp) throws SQLException;
+
+	public ObservableList<T> List() throws SQLException;
+
 }
