e03b246861d6f4af667da93584ba72e328fac992
Tiago <tiago.spana@gmail.com>
2017-11-26 17:40:04 -0200

    Correção do main

--- src/com/helpdesk/main/Main.java
+++ src/com/helpdesk/main/Main.java
@@ -2,6 +2,10 @@ package com.helpdesk.main;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.sql.SQLException;
+
+import com.helpdesk.repository.ChamadoRepository;
+import com.helpdesk.repository.UsuarioRepository;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,10 +23,20 @@ public class Main extends Application {
 	@Override
 	public void start(final Stage stage) throws Exception {
 		FXMLLoader f = new FXMLLoader();
-		final Parent fxmlRoot = (Parent) f.load(new FileInputStream(new File("src/com/helpdesk/views/principal.fxml")));
+		final Parent fxmlRoot = (Parent) f.load(new FileInputStream(new File("src/com/helpdesk/views/Login.fxml")));
 		stage.setScene(new Scene(fxmlRoot));
 		stage.show();
 		
+	}
+	
+	private void CheckDB() {
+		File f = new File("sample.db");
+		if (!f.exists()) {
+			ChamadoRepository.Initialize();
+			UsuarioRepository.Initialize();
+			
+		}
+
 	}
 
 }
