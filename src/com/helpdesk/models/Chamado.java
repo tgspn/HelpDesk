ddb8e20ab98c26583049644c12ea6ea176f43ef1
Tiago <tiago.spana@gmail.com>
2017-11-26 17:38:13 -0200

    Correções do chamado

--- src/com/helpdesk/models/Chamado.java
+++ src/com/helpdesk/models/Chamado.java
@@ -8,52 +8,19 @@ public class Chamado {
 	private String assunto;
 	private String situacao;
 	private int idCliente;
+	private Tecnico tecnico;
 	
 	
-	public Chamado(int id, String descricao, String categoria, String assunto, String situacao, int idCliente) {
+	public Chamado(int id, String descricao, String categoria, String assunto, String situacao, int idCliente,Tecnico tecnico) {
 		this.id = id;
 		this.descricao = descricao;
 		this.categoria = categoria;
 		this.assunto = assunto;
 		this.situacao = situacao;
 		this.idCliente = idCliente;
+		this.tecnico=tecnico;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public String getCategoria() {
-		return categoria;
-	}
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
-	public String getAssunto() {
-		return assunto;
-	}
-	public void setAssunto(String assunto) {
-		this.assunto = assunto;
-	}
-	public String getSituacao() {
-		return situacao;
-	}
-	public void setSituacao(String situacao) {
-		this.situacao = situacao;
-	}
-	public int getIdCliente() {
-		return idCliente;
-	}
-	public void setIdCliente(int idCliente) {
-		this.idCliente = idCliente;
-	}
+	
 	
 	public void CopyTo(Chamado destino) throws Exception {
 		if(destino==null)
@@ -65,6 +32,77 @@ public class Chamado {
 		destino.id=this.id;
 		destino.idCliente=this.idCliente;
 		destino.situacao=this.situacao;
+		this.tecnico.CopyTo(destino.tecnico);
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
 	}
 	
 	
